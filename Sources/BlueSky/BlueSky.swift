acacf06e458f6779b6c6d46435811a7f0a38cb49
carlynorama <carlynorama@gmail.com>
2022-06-25 07:16:53 -0700

    Add DocC readable comments

--- Sources/BlueSky/BlueSky.swift
+++ Sources/BlueSky/BlueSky.swift
@@ -2,14 +2,24 @@ import SwiftUI
 
 public struct BlueSky {
     public private(set) var text = "Hello, World!"
-    
+
+    /// A view that displays the temperature in Los Angeles.
+    ///
+    /// This view is the HelloWord of ``BlueSky``. It provides proof that the App's connection to WeatherKit is working.
+    /// Creating an ExampleView by putting it any view. It requires no parameters.
+    /// ```
+    /// public var body: some View {
+    ///    ExampleView()
+    /// }
+    /// ```
+    ///
     public struct ExampleView:View {
         public init() {}
         public var body: some View {
             ExampleWeatherView()
         }
     }
-    
+
 
     public init() {
     }
