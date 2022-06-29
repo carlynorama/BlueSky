9936ef17f6e2dfdf45f1d905c2fbadbd21db1498
carlynorama <carlynorama@gmail.com>
2022-06-29 11:38:57 -0700

    refine ForecastInfo to WeatherReport

---
+++ Sources/BlueSky/WeatherReporter/WeatherReport.swift
@@ -0,0 +1,202 @@
+//
+//  WeatherReport.swift
+//  Wind
+//
+//  Created by Labtanza on 6/24/22.
+//
+
+import Foundation
+import WeatherKit
+
+
+
+public struct WeatherReport {
+    let date: Date
+    let condition: String
+    let symbolName: String
+    
+    let temperature: Temperature
+    let precipitation: String?
+    let precipitationChance: Double?
+    
+    let windSpeed: Measurement<UnitSpeed>
+    let windDirection: Measurement<UnitAngle>
+    let gustSpeed: Measurement<UnitSpeed>?
+    let windCompassDirection:Wind.CompassDirection
+    let windAsStandardUnits:(direction:Double, speed:Double, gust:Double?)
+    
+    var isDailyForecast: Bool {
+        temperature.isDaily
+    }
+    
+    var isHourlyForecast: Bool {
+        !temperature.isDaily
+    }
+    
+    var age:TimeInterval {
+        date.timeIntervalSince(Date.now)
+    }
+}
+
+
+public extension WeatherReport {
+    init(_ forecast: DayWeather) {
+        date = forecast.date
+        condition = forecast.condition.description
+        symbolName = forecast.symbolName
+        temperature = .daily(
+            high: forecast.highTemperature,
+            low: forecast.lowTemperature)
+        precipitation = forecast.precipitation.description
+        precipitationChance = forecast.precipitationChance
+        windSpeed = forecast.wind.speed
+        windDirection = forecast.wind.direction
+        gustSpeed = forecast.wind.gust
+        windCompassDirection = forecast.wind.compassDirection
+        windAsStandardUnits = forecast.wind.asStandardUnits
+    }
+    
+    init(_ forecast: HourWeather) {
+        date = forecast.date
+        condition = forecast.condition.description
+        symbolName = forecast.symbolName
+        temperature = .hourly(forecast.temperature)
+        precipitation = forecast.precipitation.description
+        precipitationChance = forecast.precipitationChance
+        windSpeed = forecast.wind.speed
+        windDirection = forecast.wind.direction
+        gustSpeed = forecast.wind.gust
+        windCompassDirection = forecast.wind.compassDirection
+        windAsStandardUnits = forecast.wind.asStandardUnits
+    }
+    
+    init(_ rawForcast: Forecast<DayWeather>) {
+        let forecast = rawForcast.forecast[0]
+        date = forecast.date
+        condition = forecast.condition.description
+        symbolName = forecast.symbolName
+        temperature = .daily(
+            high: forecast.highTemperature,
+            low: forecast.lowTemperature)
+        precipitation = forecast.precipitation.description
+        precipitationChance = forecast.precipitationChance
+        windSpeed = forecast.wind.speed
+        windDirection = forecast.wind.direction
+        gustSpeed = forecast.wind.gust
+        windCompassDirection = forecast.wind.compassDirection
+        windAsStandardUnits = forecast.wind.asStandardUnits
+    }
+    
+    init(_ currentWeather: CurrentWeather) {
+        let forecast = currentWeather
+        date = forecast.date
+        condition = forecast.condition.description
+        symbolName = forecast.symbolName
+        temperature = .hourly(forecast.temperature)
+        precipitation = nil
+        precipitationChance = nil
+        windSpeed = forecast.wind.speed
+        windDirection = forecast.wind.direction
+        gustSpeed = forecast.wind.gust
+        windCompassDirection = forecast.wind.compassDirection
+        windAsStandardUnits = forecast.wind.asStandardUnits
+    }
+}
+
+enum Temperature {
+    typealias Value = Measurement<UnitTemperature>
+    
+    case daily(high: Value, low: Value)
+    case hourly(Value)
+    
+    var isDaily: Bool {
+        switch self {
+        case .daily:
+            return true
+        case .hourly:
+            return false
+        }
+    }
+}
+
+
+extension Temperature: Hashable {
+    func hash(into hasher: inout Hasher) {
+        switch self {
+        case let .daily(high, low):
+            hasher.combine(0)
+            hasher.combine(high)
+            hasher.combine(low)
+        case let .hourly(temp):
+            hasher.combine(1)
+            hasher.combine(temp)
+        }
+    }
+}
+
+#if DEBUG
+// Use this for preview data.
+extension WeatherReport {
+    static var daily: WeatherReport {
+        WeatherReport(
+            date: .now,
+            condition: condition,
+            symbolName: "cloud.sun.rain",
+            temperature: dailyTemperature,
+            precipitation: "rain",
+            precipitationChance: 0.15,
+            windSpeed: windSpeed,
+            windDirection: windDirection,
+            gustSpeed: gustSpeed,
+            windCompassDirection: Wind.CompassDirection.north,
+            windAsStandardUnits: (direction: 0.0, speed: 4.2, gust: 5.2)
+        )
+    }
+    
+    static var hourly: WeatherReport {
+        WeatherReport(
+            date: .now,
+            condition: condition,
+            symbolName: "cloud.sun.rain",
+            temperature: hourlyTemperature,
+            precipitation: "rain",
+            precipitationChance: 0.15,
+            windSpeed: windSpeed,
+            windDirection: windDirection,
+            gustSpeed: gustSpeed,
+            windCompassDirection: Wind.CompassDirection.north,
+            windAsStandardUnits: (direction: 0.0, speed: 4.2, gust: 5.2)
+        )
+    }
+    
+    private static let condition =
+    """
+    Lorem ipsum dolor sit amet, \
+    consectetur adipiscing elit."
+    """
+    
+    private static var hourlyTemperature: Temperature = {
+        let temp = Measurement<UnitTemperature>(value: 60.1, unit: .fahrenheit)
+        return .hourly(temp)
+    }()
+
+    private static var dailyTemperature: Temperature = {
+        let high = Measurement<UnitTemperature>(value: 81.7, unit: .fahrenheit)
+        let low = Measurement<UnitTemperature>(value: 52.4, unit: .fahrenheit)
+        return .daily(high: high, low: low)
+    }()
+
+    private static var windSpeed: Measurement<UnitSpeed> = {
+        Measurement<UnitSpeed>(value: 4.2, unit: .milesPerHour)
+    }()
+    
+    private static var windDirection: Measurement<UnitAngle> = {
+        Measurement<UnitAngle>(value: 0.0, unit: .radians)
+    }()
+    
+    private static var gustSpeed: Measurement<UnitSpeed> = {
+        Measurement<UnitSpeed>(value: 5.2, unit: .milesPerHour)
+    }()
+}
+#endif
+
