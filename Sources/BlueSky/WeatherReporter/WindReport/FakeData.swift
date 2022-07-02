baca44a5881ebc82486a98fefa76744e68dedd26
carlynorama <carlynorama@gmail.com>
2022-07-02 04:54:48 -0700

    moved all fake report generation to wind report

--- Sources/BlueSky/WeatherReporter/WindReport/FakeData.swift
+++ Sources/BlueSky/WeatherReporter/WindReport/FakeData.swift
@@ -13,23 +13,32 @@ import WeatherKit
 
 
 extension WindReport {
-    static var example:WindReport {
-        WindReport(
-            date: Date.now,
-            direction: Measurement<UnitAngle>(value: 23, unit: .degrees),
-            speed: Measurement<UnitSpeed>(value: 5, unit: .knots),
-            gustSpeed: Measurement<UnitSpeed>(value: 10, unit: .knots))
+    public static var example:WindReport {
+        let levelRaw = Int.random(in: 0...9)
+        let windLevel = WindScaleValue(rawValue: levelRaw)!
+        return example(for: windLevel)
     }
-}
-
-extension WindScaleValue {
     
-    static func randomSpeedsForLevel(_ level:Self) -> (average:Measurement<UnitSpeed>, gust:Measurement<UnitSpeed>?) {
-        let speedRange = level.windSpeedMin.value..<level.windSpeedMax.value
+    public static func example(for windLevel:WindScaleValue) -> WindReport{
+        let speeds = randomSpeeds(for:windLevel)
+
+        return WindReport(
+            date: Date.now,
+            direction: randomDirection(),
+            speed: speeds.average,
+            gustSpeed: speeds.gust)
+    }
+    
+    static func randomDirection() -> Measurement<UnitAngle> {
+        return Measurement<UnitAngle>(value:Double.random(in:(0.0...360.0)), unit: .degrees)
+    }
+    
+    static func randomSpeeds(for windLevel:WindScaleValue) -> (average:Measurement<UnitSpeed>, gust:Measurement<UnitSpeed>?) {
+        let speedRange = windLevel.windSpeedMin.value..<windLevel.windSpeedMax.value
         var gustSpeed:Double?
         let speed = Double.random(in:speedRange)
         if (Bool.random()) {
-            let lb = (speedRange.upperBound-speedRange.lowerBound)/2
+            let lb = speed
             let up = (speedRange.upperBound * 1.25)
             let gustRange = lb...up
             gustSpeed = Double.random(in: gustRange)
@@ -43,21 +52,7 @@ extension WindScaleValue {
             return (average:Measurement<UnitSpeed>(value: speed, unit: .knots), gust:nil)
         }
     }
-    
-    static func randomDirectionForLevel(_ level:Self) -> Measurement<UnitAngle> {
-        return Measurement<UnitAngle>(value:Double.random(in:(0.0...360.0)), unit: .degrees)
-    }
-    
-    public static func exampleWindReportForLevel(_ level:Self) -> WindReport{
-        let speeds = randomSpeedsForLevel(level)
-        return WindReport(
-            date: Date.now,
-            direction: randomDirectionForLevel(level),
-            speed: speeds.average,
-            gustSpeed: speeds.gust)
-    }
 }
-
     
 #endif
 
